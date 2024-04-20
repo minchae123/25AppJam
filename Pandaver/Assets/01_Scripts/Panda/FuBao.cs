27d84d8c4661d88d11b44c855417fbfab1a0bf7d
minchae123 <08minchae17@gmail.com>
2024-04-20 16:38:32 +0900

    Dotween

---
+++ Pandaver/Assets/01_Scripts/Panda/FuBao.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class FuBao : MonoBehaviour
+{
+	private void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.D))
+		{
+			MoveFuBao(3);
+		}
+	}
+
+	public void MoveFuBao(int level)
+	{
+		transform.DOLocalMoveX(transform.localPosition.x + level * -10f, 1f);
+	}
+}
